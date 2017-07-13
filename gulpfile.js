02607c2cfaa0e266f2524d8c6d4403d0fd81dd11
Anthony <anthonylombardo81@gmail.com>
2017-07-12 20:45:57 -0400

    Added CoffeeScript files

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,12 @@
-var gulp = require('gulp'), // bring in the gulp library
-    gutil = require('gulp-util');
+var gulp = require('gulp'),// bring in the gulp library
+    gutil = require('gulp-util'),
+    coffee = require('gulp-coffee');
 
-gulp.task('log', function() {
-  gutil.log('Workflows are awesome');
+var coffeeSources = ['components/coffee/tagline.coffee']; // can usee wildcard before extension
+
+gulp.task('coffee', function() {
+  gulp.src(coffeeSources)
+    .pipe(coffee({ bare: true }) // bare = compiles JavaScript without putting it in a safety wrapper like it normally would
+      .on('error', gutil.log))
+    .pipe(gulp.dest('components/scripts')) // once it's finish processing the coffee script, put new file in this location.
 });
