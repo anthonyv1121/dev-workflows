127904e844e63066773505f6967cdacc5c35a192
Anthony <anthonylombardo81@gmail.com>
2017-07-12 21:00:34 -0400

    Concat JS files

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,25 @@
 var gulp = require('gulp'),// bring in the gulp library
     gutil = require('gulp-util'),
-    coffee = require('gulp-coffee');
+    coffee = require('gulp-coffee'),
+    concat = require('gulp-concat');
 
-var coffeeSources = ['components/coffee/tagline.coffee']; // can usee wildcard before extension
+var coffeeSources = ['components/coffee/tagline.coffee'];
+var jsSources = [ // array of all JS documents - oder of processing is based on order in array
+  'components/scripts/rclick.js',
+  'components/scripts/pixgrid.js',
+  'components/scripts/tagline.js',
+  'components/scripts/template.js'
+];
 
 gulp.task('coffee', function() {
   gulp.src(coffeeSources)
     .pipe(coffee({ bare: true }) // bare = compiles JavaScript without putting it in a safety wrapper like it normally would
       .on('error', gutil.log))
     .pipe(gulp.dest('components/scripts')) // once it's finish processing the coffee script, put new file in this location.
+});
+
+gulp.task('js', function() {
+  gulp.src(jsSources)
+    .pipe(concat('script.js')) // the name of the file we want to build
+    .pipe(gulp.dest('builds/development/js')) // destination of file
 });
